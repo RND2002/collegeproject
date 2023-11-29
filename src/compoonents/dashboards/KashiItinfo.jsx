18fa377105b4c93735e680f8ead8ca7b6c032aa9
Aryan Dwivedi <aryan.735921@gmail.comm>
2023-11-29 20:27:57 +0530

    fdgdf

---
+++ src/compoonents/dashboards/KashiItinfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './KashiItInfo.css'
+const KashiItinfo = () => {
+  return (
+    <div>
+        Roshan write here
+    </div>
+  )
+}
+
+export default KashiItinfo
